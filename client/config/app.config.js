2fe3623b88ae05473bd1ae863650371e49fa5ca2
Razi Alawamleh <razi.alawamleh@gmail.com>
2024-05-19 13:08:36 -0400

    updated dependencies and app.config.js

--- client/config/app.config.js
+++ client/config/app.config.js
@@ -7,5 +7,10 @@ export default ({ config }) => {
       apiUrl: process.env.EXPO_PUBLIC_API_URL,
       clerkPublishableKey: process.env.EXPO_PUBLIC_CLERK_PUBLISHABLE_KEY,
     },
+    jsEngine: "jsc", // Use JavaScriptCore instead of Hermes
+    icon: "./assets/icon.png",
+    splash: {
+      image: "./assets/splash.png",
+    },
   };
 };
