66fd9ef13d850c56c15d05e46a459121a2f9625c
Razi Alawamleh <razi.alawamleh@gmail.com>
2024-05-20 21:21:39 -0400

    fixed minssing transform routerroot error.

--- client/config/metro.config.js
+++ client/config/metro.config.js
@@ -1,9 +1,17 @@
 const { getDefaultConfig } = require('expo/metro-config');
 
-module.exports = (async () => {
-    const defaultConfig = await getDefaultConfig(__dirname);
+// module.exports = (async () => {
+//     const defaultConfig = await getDefaultConfig(__dirname);
 
-    defaultConfig.resolver.assetExts.push('ts', 'tsx', 'js', 'jsx', 'json');
+//     defaultConfig.resolver.assetExts.push('ts', 'tsx', 'js', 'jsx', 'json');
 
-    return defaultConfig;
-})();
+//     return defaultConfig;
+// })();
+
+const { getDefaultConfig } = require('expo/metro-config');
+
+/** @type {import('expo/metro-config').MetroConfig} */
+const defaultConfig = getDefaultConfig(__dirname);
+defaultConfig.resolver.sourceExts.push('cjs');
+
+module.exports = defaultConfig;
