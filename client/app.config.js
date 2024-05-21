f83654cfdac43656faa7d3e4a192764c8409f254
Razi Alawamleh <razi.alawamleh@gmail.com>
2024-05-21 10:24:33 -0400

    replaced app.json w/  app.config.js

--- client/app.config.js
+++ client/app.config.js
@@ -2,15 +2,39 @@ import 'dotenv/config';
 
 export default ({ config }) => {
   return {
-    ...config,
-    extra: {
-      apiUrl: process.env.EXPO_PUBLIC_API_URL,
-      clerkPublishableKey: process.env.EXPO_PUBLIC_CLERK_PUBLISHABLE_KEY,
-    },
-    jsEngine: "jsc", // Ensure JavaScriptCore is used instead of Hermes
+    name: "Family Traditions",
+    slug: "FamilyTraditions",
+    version: "1.0.0",
+    orientation: "portrait",
+    userInterfaceStyle: "light",
+    jsEngine: "jsc",
     icon: "./assets/icon.png",
     splash: {
       image: "./assets/splash.png",
+      resizeMode: "contain",
+      backgroundColor: "#ffffff"
+    },
+    assetBundlePatterns: ["**/*"],
+    ios: {
+      supportsTablet: true,
+      jsEngine: "jsc"
+    },
+    android: {
+      adaptiveIcon: {
+        foregroundImage: "./assets/adaptive-icon.png",
+        backgroundColor: "#ffffff"
+      }
+    },
+    web: {
+      bundler: "metro",
+      favicon: "./assets/favicon.png"
+    },
+    plugins: [
+      ["expo-router", { "root": "./src" }]
+    ],
+    extra: {
+      apiUrl: process.env.EXPO_PUBLIC_API_URL,
+      clerkPublishableKey: process.env.EXPO_PUBLIC_CLERK_PUBLISHABLE_KEY,
     },
   };
 };
