d00846a8291b77eaa83c1bc59e400d139c6adc31
Razi Alawamleh <razi.alawamleh@gmail.com>
2024-05-26 02:39:00 -0400

    connected to server successfully, good night.

--- server/server.js
+++ server/server.js
@@ -1,27 +1,25 @@
 const express = require('express');
 const { Pool } = require('pg');
-
-// Load environment variables (for local development)
 require('dotenv').config();
-// Setup PostgreSQL connection pool using environment variables
 
 const pool = new Pool({
   connectionString: process.env.DATABASE_URL,
-  ssl: true
-  });
+  ssl: true,
+});
 
-// Create an instance of express application
 const app = express();
-
-// Use express built-in middleware to parse JSON request bodies
 app.use(express.json());
 
-// Import and use routes modules, passing the Express app and PostgreSQL pool
-require('./routes/userRoutes')(app, pool);
-require('./routes/recipeBooksRoutes')(app, pool);
-require('./routes/recipesRoutes')(app, pool);
+// Import routes
+const userRoutes = require('./routes/userRoutes');
+const recipeBookRoutes = require('./routes/recipeBooksRoutes');
+const recipeRoutes = require('./routes/recipesRoutes');
 
-// Specify the port to listen on, use environment variable or default to 3000
+// Use routes
+app.use('/api/users', userRoutes);
+app.use('/api/recipeBooks', recipeBookRoutes);
+app.use('/api/recipes', recipeRoutes);
+
 const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => {
     console.log(`Server running on port ${PORT}`);
