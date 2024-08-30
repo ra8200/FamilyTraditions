8982ec31c6e5931acd51e30de30841a6ed4396a0
Razi Alawamleh <razi.alawamleh@gmail.com>
2024-08-30 10:27:18 -0400

    Update server.js

--- server/server.js
+++ server/server.js
@@ -37,7 +37,6 @@ app.get('/', (req, res) => {
   res.send('Welcome to Recipe Book API');
 });
 
-const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
 });
